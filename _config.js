6b2f766c09c3c63f73774282ebffd38c83d43270
Michael Prather <mprather1@gmail.com>
2016-10-27 19:48:23 -0500

    new

---
+++ _config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.mongoURI = {
+  development: 'mongodb://localhost/database',
+  production: 'mongodb://localhost/database',
+  test: 'mongodb://localhost/database'
+};
+
+module.exports = config;
